12a3a5c1e66a5860ef60bfae30776415e829662c
Keshav Agrawal <Keshavagrawal9471408042@gmail.com>
2024-08-21 06:26:06 +0530

    set login form initial values to demo user for presentation purposes

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@ex.com");
+  const [password, setPassword] = useState("Qwerty12");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
